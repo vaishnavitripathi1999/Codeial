2d41c41553f020fa8724a959582178aa4185d086
vaishnavitripathi1999 <simanttripathi@gmail.com>
2020-06-24 15:37:00 +0530

    updated

--- codeial/routes/index.js
+++ codeial/routes/index.js
@@ -2,9 +2,16 @@
 //getting the express module 
 const express= require('express');
 
+//importing the homecontroller 
+const homecontroller = require('../controllers/home_controller');
+
 
 //calling router of express in a variable called router 
 const router = express.Router();
+
+
+//accessing the home from homecontroller
+router.get('/',homecontroller.home);
 
 
 console.log('index from the router is loaded');
